1706a32f22819f7235537df8a553f517c3755f8b
Ridoy Roy <ridoyroycse53engg@gmail.com>
2022-02-10 03:17:00 +0600

    Search functionality and readme added

--- mystore/urls.py
+++ mystore/urls.py
@@ -3,8 +3,10 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.mystore, name= 'mystore'),
-    path('<slug:category_slug>/', views.mystore, name= 'products_by_category'),
-    path('<slug:category_slug>/<slug:product_slug>', views.product_detail, name= 'product_detail'),
+    path('', views.mystore, name='mystore'),
+    path('category/<slug:category_slug>/', views.mystore, name='products_by_category'),
+    path('category/<slug:category_slug>/<slug:product_slug>',
+         views.product_detail, name='product_detail'),
+    path('search/', views.search, name="search"),
 
 ]
