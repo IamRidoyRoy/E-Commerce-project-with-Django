ec10b24d81f60da5f35d48d61a94b2c9546da345
Ridoy Roy <ridoyroycse53engg@gmail.com>
2022-02-14 19:51:04 +0600

    Product Variation Model added

--- carts/views.py
+++ carts/views.py
@@ -15,6 +15,10 @@ def _cart_id(request):
     return cart
 
 def add_cart(request, product_id):
+    color = request.GET['color']
+    size = request.GET['size']
+    return HttpResponse(color + ' ' + size)
+    exit()
     product = Product.objects.get(id = product_id) # By using it we get the product.
     try:
         cart = Cart.objects.get(cart_id= _cart_id(request) ) # get cart using cart id in the session
@@ -26,7 +30,7 @@ def add_cart(request, product_id):
 
     try:
         cart_item = CartItem.objects.get(product = product, cart= cart)
-        cart_item.quantity += 1 #cart quantuty will incremet by one
+        cart_item.quantity += 1 #cart quantuty will increment by one
         cart_item.save()
     except CartItem.DoesNotExist:
         cart_item = CartItem.objects.create(
--- mystore/models.py
+++ mystore/models.py
@@ -1,3 +1,4 @@
+from tkinter import CASCADE
 from django.db import models
 from django.db.models.fields.related import ForeignKey
 
@@ -24,3 +25,19 @@ class Product(models.Model):
 
     def __str__(self):
         return self.product_name
+
+# Create a tuple for drop down choices 
+variation_category_choice = (
+    ('color', 'color'),
+    ('size', 'size'),
+)
+# Create Variation class 
+class Variation(models.Model):
+    product             = models.ForeignKey(Product, on_delete=models.CASCADE)
+    variation_category  = models.CharField(max_length=100, choices=variation_category_choice)
+    variation_value     = models.CharField(max_length=100)
+    is_active           = models.BooleanField(default=True) 
+    created_date        = models.DateTimeField(auto_now=True)
+
+    def __unicode__(self):
+        return self.product
