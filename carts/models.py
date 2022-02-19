ed607ba4debec15a0b8163ecb0d41c6f4adf5b0b
Ridoy Roy <ridoyroycse53engg@gmail.com>
2022-02-20 05:15:12 +0600

    Stores and cart functionality

--- carts/models.py
+++ carts/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.db.models.fields.related import ForeignKey
 
-from mystore.models import Product
+from mystore.models import Product, Variation
 
 # Create your models here.
 
@@ -14,6 +14,7 @@ class Cart(models.Model):
 
 class CartItem(models.Model):
     product     = models.ForeignKey(Product, on_delete = models.CASCADE)
+    variations  = models.ManyToManyField(Variation, blank=False)
     cart        = models.ForeignKey(Cart, on_delete = models.CASCADE )
     quantity    = models.IntegerField()
     is_active   = models.BooleanField(default = True)
@@ -22,6 +23,6 @@ class CartItem(models.Model):
         return self.product.price *self.quantity
 
 
-    def __str__(self):
+    def __unicode__(self):
         return self.product  
 
--- mystore/models.py
+++ mystore/models.py
@@ -48,7 +48,7 @@ class Variation(models.Model):
     is_active           = models.BooleanField(default=True) 
     created_date        = models.DateTimeField(auto_now=True)
 
-    object = VariationManager()
+    objects = VariationManager()
 
-    def __unicode__(self):
-        return self.product
+    def __str__(self):
+        return self.variation_value
