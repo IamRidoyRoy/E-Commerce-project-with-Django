c0c3a6c964d21fb8c24f719c5d11c7514aa12998
Ridoy Roy <ridoyroycse53engg@gmail.com>
2022-03-07 21:23:28 +0600

    Design checkout page and fix checkout bug

--- carts/views.py
+++ carts/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import get_object_or_404, redirect, render
 from .models import CartItem, Cart
 
 from mystore.models import Product, Variation
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -90,6 +91,7 @@ def add_cart(request, product_id):
     return redirect('cart')
 
 
+# Cart page details
 def remove_cart(request, product_id, cart_item_id):
     cart = Cart.objects.get(cart_id=_cart_id(request))
     product = get_object_or_404(Product, id=product_id)
@@ -140,3 +142,30 @@ def cart(request, total=0, quantity=0, cart_items=None, tax=0, grand_total=0):
     }
 
     return render(request, 'mystore/cart.html', context)
+
+
+# Checkout page details
+@login_required(login_url='login')
+def checkout(request, total=0, quantity=0, cart_items=None):
+    try:
+        tax = 0
+        grand_total = 0
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+        cart_items = CartItem.objects.filter(cart=cart, is_active=True)
+        for cart_item in cart_items:
+            total += (cart_item.product.price * cart_item.quantity)
+            quantity += cart_item.quantity
+
+        tax = (1*total)/100
+        grand_total = total + tax
+    except:
+        pass
+
+    context = {
+        'total': total,
+        'quantity': quantity,
+        'cart_items': cart_items,
+        'tax': tax,
+        'grand_total': grand_total,
+    }
+    return render(request, 'mystore/checkout.html', context)
